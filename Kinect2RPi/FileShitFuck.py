3e36f934ed324e1e4a0319f067b82c3596b70298
John Naylor <jonaylor89@gmail.com>
2018-04-08 02:13:41 -0400

    Write to file

---
+++ Kinect2RPi/FileShitFuck.py
@@ -0,0 +1,16 @@
+
+def get_head():
+    values = [0, 0, 0]
+    with open(head.dat) as f:
+        line = f.readline()
+        if line != '':
+            values = line.split(',')
+
+    return values
+
+
+if __name__ == '__main__':
+    from time import sleep
+    while True:
+        print(get_head())
+        sleep(0.1)
